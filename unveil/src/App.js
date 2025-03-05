beb33046ba17bac8dc02205c502388cd33f0b0a5
Ashini Upeksha <165469241+ashiUpeksha@users.noreply.github.com>
2025-03-05 19:15:39 +0530

    First Commit

---
+++ unveil/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Material UI</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
