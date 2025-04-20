cea09d275b6475905a73d48188aa803926f04c73
Ashini Upeksha <165469241+ashiUpeksha@users.noreply.github.com>
2025-04-20 18:26:27 +0530

    Update App.js
    Update App.js

--- unveil/src/App.js
+++ unveil/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import RequestToSignUp from './views/RequestToSignUp/RequestToSignUp';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./views/HomePage/HomePage";
+import AddNewEvent from "./views/AddNewEvent/AddNewEvent";
+import AddImagesOrVideos from "./views/AddImagesOrVideos/AddImagesOrVideos";
+import ViewEvents from "./views/ViewEvents/ViewEvents";
+import UpdateEvents from "./views/UpdateEvents/UpdateEvents";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <RequestToSignUp/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/addnewevent" element={<AddNewEvent />} /> 
+        <Route path="/addimageorvideos" element={<AddImagesOrVideos />} /> 
+        <Route path="/viewevents" element={<ViewEvents />} /> 
+        <Route path="/updateevents" element={<UpdateEvents />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
