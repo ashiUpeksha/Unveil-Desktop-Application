cffbd3df51d90e31658a2e2a1580e4ac8462dbe0
Ashini Upeksha <165469241+ashiUpeksha@users.noreply.github.com>
2025-05-15 22:59:05 +0530

    updated nav bar

--- unveil/src/components/Navbar.js
+++ unveil/src/components/Navbar.js
@@ -2,22 +2,39 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useEffect } from "react";
 
 const NavBar = () => {
+  // Get user info from localStorage
+  const [organization, setOrganization] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  useEffect(() => {
+    // Parse localStorage.user as JSON
+      const userObj = JSON.parse(localStorage.user || "{}");
+      setOrganization(userObj.organization || "");
+      setUsername(userObj.username || "");
+    
+  }, []);
+
   return (
     <AppBar
       position="fixed"
       sx={{
         backgroundColor: "#4F4F4F",
         zIndex: (theme) => theme.zIndex.drawer + 1,
+        px: 2,
       }}
     >
-      <Toolbar>
-        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-          <Link to="/home" style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* Left section: Logo and App Name */}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Link
+            to="/home"
+            style={{ display: "flex", alignItems: "center", textDecoration: "none" }}
+          >
             <img
               src="/static/logo.png"
               alt="Unveil Logo"
@@ -38,8 +55,19 @@ const NavBar = () => {
             U&nbsp;&nbsp;n&nbsp;&nbsp;v&nbsp;&nbsp;e&nbsp;&nbsp;i&nbsp;&nbsp;l
           </Typography>
         </Box>
-        <Button color="inherit" component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/about">About</Button>
+
+        {/* Right section: Org Info and Profile Icon */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Box sx={{ textAlign: "right" }}>
+            <Typography variant="body1" sx={{ fontWeight: "bold", color: "#fff" }}>
+              {organization}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "#ccc", fontSize: "0.85rem" }}>
+              {username}
+            </Typography>
+          </Box>
+          <AccountCircleIcon sx={{ color: "#fff", fontSize: 30 }} />
+        </Box>
       </Toolbar>
     </AppBar>
   );
